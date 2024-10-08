51c2e7c7d438fe32ad12b7c39607f3ffd594a9fb
Aayush Dhungel <adhungel0413@gmail.com>
2024-10-08 22:44:57 +0545

    adding aggregation pipelines(mongodb)

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -128,3 +128,97 @@ exports.deleteTour = async (req, res) => {
     })
   }
 }
+
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: {
+          ratingsAverage: {
+            $gte: 4.5,
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $toUpper: "$difficulty" },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: "$ratingsQuantity" },
+          avgRating: { $avg: "$ratingsAverage" },
+          avgPrice: { $avg: "$price" },
+          minPrice: { $min: "$price" },
+          maxPrice: { $max: "$price" },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: "easy" } },
+      // },
+    ])
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        stats,
+      },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      message: err,
+    })
+  }
+}
+
+exports.getMonthlyPlan = async (req, res) => {
+  try {
+    const year = req.params.year * 1 // 2021
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: "$startDates",
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
+          },
+        },
+      },
+      {
+        $group: {
+          _id: { $month: "$startDates" },
+          numTourStarts: { $sum: 1 },
+          tours: { $push: "$name" },
+        },
+      },
+      {
+        $addFields: { month: "$_id" },
+      },
+      {
+        $project: { _id: 0 }, // 0 to not show/project; 1 otherwise
+      },
+      {
+        $sort: { month: 1 }, // 1 for ascending; -1 for descending
+      },
+      {
+        $limit: 12, // just for info; maximum 12 value for each of the 12 months
+      },
+    ])
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        plan,
+      },
+    })
+  } catch (err) {
+    res.status(400).json({
+      status: "failure",
+      message: err,
+    })
+  }
+}
