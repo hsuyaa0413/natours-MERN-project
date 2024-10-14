50b4f6a365d2f643fdcb8fb6528f81b49af6b055
Aayush Dhungel <adhungel0413@gmail.com>
2024-10-14 13:23:37 +0545

    implementing globalErrorHandler

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express")
 const morgan = require("morgan")
 
+const AppError = require("./utils/appError")
+const globalErrorHandler = require("./controllers/errorController")
 const tourRouter = require("./routes/tourRoutes")
 const userRouter = require("./routes/userRoutes")
 
@@ -28,11 +30,15 @@ app.use((req, res, next) => {
 app.use("/api/v1/tours", tourRouter)
 app.use("/api/v1/users", userRouter)
 
+// handling unhandled routes
 app.all("*", (req, res, next) => {
-  res.status(404).json({
-    status: "failure",
-    message: `Can't find ${req.originalUrl} on this server!`,
-  })
+  // const err = new Error(`Can't find ${req.originalUrl} on this server!`)
+  // err.statusCode = 404
+  // err.status = "failure"
+
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
 })
+
+app.use(globalErrorHandler)
 
 module.exports = app
