61c30581e3dab183ef4edde75128f0bbd3610eec
Aayush Dhungel <adhungel0413@gmail.com>
2024-10-24 23:52:46 +0545

    sanitizing data & preventing parameter pollution

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ const express = require("express")
 const morgan = require("morgan")
 const rateLimit = require("express-rate-limit")
 const helmet = require("helmet")
+const mongoSanitize = require("express-mongo-sanitize")
+const { xss } = require("express-xss-sanitizer")
+const hpp = require("hpp")
 
 const AppError = require("./utils/appError")
 const globalErrorHandler = require("./controllers/errorController")
@@ -29,6 +32,26 @@ app.use("/api", limiter)
 
 // body parser; reads data from body into req.body
 app.use(express.json({ limit: "10kb" }))
+
+// data sanitization against NoSQL query injection
+app.use(mongoSanitize())
+
+// data sanitization against XSS
+app.use(xss())
+
+// prevent parameter pollution
+app.use(
+  hpp({
+    whitelist: [
+      "duration",
+      "ratingsAverage",
+      "ratingsQuantity",
+      "maxGroupSize",
+      "difficulty",
+      "price",
+    ],
+  })
+)
 
 // serving static files
 app.use(express.static(`${__dirname}/public`))
